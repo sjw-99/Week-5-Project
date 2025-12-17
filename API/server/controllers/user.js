4586265a322e1c7530a7f18057c996f169e0edd7
jostling01 <james.ostling@me.com>
2025-12-17 11:36:56 +0000

    add login function to user controller, add code to register.js on front end, add getOneByUsername class method to User model

--- API/server/controllers/user.js
+++ API/server/controllers/user.js
@@ -13,3 +13,21 @@ async function register(req, res) {
         res.status(400).json({ error: err.message })
     }
 }
+
+async function login(req, res) {
+    try {
+        const data = req.body;
+        const user = await User.getOneByUsername(data.username)
+        
+        const match = await bcrypt.compare(data.password, user.password)
+        if (match) {
+            res.status(200).json({"success": true})
+        } else {
+            throw new Error('User could not be authenticated')
+        }
+
+        res.status(200).json(data)
+    } catch(err) {
+        res.status(401).json({ error: err.message})
+    }
+}
--- API/server/models/User.js
+++ API/server/models/User.js
@@ -17,13 +17,21 @@ class User {
         return new User(response.rows[0]);
     }
 
+    static async getOneByUsername(username) {
+        const response = await db.query("SELECT * FROM user_account WHERE username = $1", [username]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate user.");
+        }
+        return new User(response.rows[0]);
+    }
+
     static async create(data) {
         const { username, password, isAdmin } = data;
         let response = await db.query("INSERT INTO user_account (username, password) VALUES ($1, $2) RETURNING user_id;",
             [username, password]); // inserts the users entered username and password into the database
         const newId = response.rows[0].user_id; // returns the user id
         const newUser = await User.getOneById(newId); // run the getOneById class method which returns a new instance of our User class
-        return newUser;
+        return newUser; // return the new instance of User class
     }
 }
 
