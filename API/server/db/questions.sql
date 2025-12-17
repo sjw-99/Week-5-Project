1c10a1e426fc3e3a72bab827323429703211bbc5
jostling01 <james.ostling@me.com>
2025-12-17 10:41:09 +0000

    add user_account table to sql file

--- API/server/db/questions.sql
+++ API/server/db/questions.sql
@@ -54,3 +54,11 @@ CREATE TABLE stats (
     topic_percent FLOAT,
     times_completed INT
 );
+
+DROP TABLE IF Exists user_account;
+CREATE TABLE user_account (
+    user_id INT GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password CHAR(60) NOT NULL,
+    PRIMARY KEY (user_id)
+);
