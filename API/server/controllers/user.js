4749747c6ceb63d3ee9cbefe6d466e185d5d767c
jostling01 <james.ostling@me.com>
2025-12-17 21:13:48 +0000

    update login and register js and html files in frontend

--- API/server/controllers/user.js
+++ API/server/controllers/user.js
@@ -8,7 +8,7 @@ async function register(req, res) {
         const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS)); // generate a random salt
         data['password'] = await bcrypt.hash(data.password, salt); // assign the salt to data['password'] and hash them together
         const result = await User.create(data)
-
+        res.status(201).send(result);
     } catch (err) {
         res.status(400).json({ error: err.message })
     }
