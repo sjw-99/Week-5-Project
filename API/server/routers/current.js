d937316342823494bd61ca8eddaf4c6f70a01ccd
Raza <rsc1099@gmail.com>
2025-12-17 01:13:10 +0000

    modified current routers

--- API/server/routers/current.js
+++ API/server/routers/current.js
@@ -4,7 +4,6 @@ const currentController = require('../controllers/current')
 const currentRouter = Router() 
 
 currentRouter.get('/questions/summary', currentController.index)
-currentRouter.get('/QN/:', currentController.show)
 currentRouter.patch("/question/:question_id", currentController.update);
 
 module.exports = currentRouter;
