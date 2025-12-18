d2f7d934723479da41569c87fc57fb81b9f220fe
Raza <rsc1099@gmail.com>
2025-12-18 11:22:55 +0000

    sort summary feature

--- API/server/controllers/current.js
+++ API/server/controllers/current.js
@@ -6,8 +6,9 @@ async function index(req, res) {
     try {
         const current_mission = await Current.getAll();
         res.status(200).json(current_mission);
-        await Student.addScoreToStudent()
+        const scores = await Student.addScoreToStudent()
         await Current.clearTable()
+        
     } catch(err) {
         res.status(500).json({'error test': err.message})
     }
